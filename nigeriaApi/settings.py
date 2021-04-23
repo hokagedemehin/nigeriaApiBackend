5eb08c5a4c983b0f2b9d4ec552ed37109a9aabca
Demehin Ibukunoluwa George <ibk2k7@gmail.com>
2021-04-23 19:53:09 +0100

    removed credential info from settings.py

--- nigeriaApi/settings.py
+++ nigeriaApi/settings.py
@@ -168,19 +168,6 @@ STATIC_URL = f"https://storage.cloud.google.com/{GS_BUCKET_NAME}/static/"
 
 from google.oauth2 import service_account
 
-credentials={
-  "type": os.environ["type"],
-  "project_id": os.environ["project_id"],
-  "private_key_id": os.environ["private_key_id"],
-  "private_key": os.environ["private_key"],
-  "client_email": os.environ["client_email"],
-  "client_id": os.environ["client_id"],
-  "auth_uri": os.environ["auth_uri"],
-  "token_uri": os.environ["token_uri"],
-  "auth_provider_x509_cert_url": os.environ["auth_provider_x509_cert_url"],
-  "client_x509_cert_url": os.environ["client_x509_cert_url"]
-}
-
 # print(credentials)
 GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
     os.path.join(BASE_DIR, 'google-credentials.json')
